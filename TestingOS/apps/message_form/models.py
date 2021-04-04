f886f6a72e868cc30772bfd100eeca7a26cf95bc
Lancer <932795170@qq.com>
2021-04-05 00:10:27 +0800

    添加留言时间

--- TestingOS/apps/message_form/models.py
+++ TestingOS/apps/message_form/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 
@@ -7,6 +7,8 @@ class Message(models.Model):
     name = models.CharField(max_length=20, verbose_name="姓名")
     email = models.EmailField(verbose_name="邮箱")
     message = models.TextField(verbose_name="留言信息")
+    # m_date = models.DateField(verbose_name="留言时间", default=datetime.now)
+    m_time = models.DateTimeField(verbose_name="留言时间", default=datetime.now)
 
     class Meta:
         verbose_name = "留言信息"
